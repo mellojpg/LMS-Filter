4cfa0c8dcb9421abd51f10f07e346d517fa163bb
alfr3dexe <alfredcastaneda32@gmail.com>
2026-06-29 18:46:10 -0700

    Base RLS class

---
+++ RlsFilter.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+clas RLS:
+	def __init__(self, n_features, forgetting= 0.99, delta=1000):
+		self.lambda_ = forgetting
+		self.w = np.zeroes((n_features, 1))
+		self.P = delta * np.eye(n_features)
+	
+	def update(self, x, d):
+		x = x.reshape(-1, 1)
+
+		# Gain vector
+		Px = self.P @ x
+		g = Px / (self.lambda_ + x.T @ Px)
+
+		# Prediction
+		y = float(self.w.T @ x)
+		
+		# Error
+		e = d - y
+		
+		# Update Weights
+		self.w += g * e
+		
+		# Update Covariance
+		self.P = (self.P - g @ x.T @ self.P) / self.lambda_
+
+		return y, e
