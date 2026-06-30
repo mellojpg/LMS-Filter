f2819d981d586c7026786f72d4b8174563028a47
mellojpg <melloj1005@gmail.com>
2026-06-29 18:44:51 -0700

    Finished example of filter working using sin waves

--- LmsFilter.py
+++ LmsFilter.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#############################################################################
+#                                                                           #
+#    IN ORDER TO GET LIBRARIES TO WORK, FOR WINDOWS RUN FOLLOWING COMMAND:  #
+#       pip install numpy matplotlib                                        #
+#                                                                           #
+#############################################################################
+
 class LMSFilter:
     def __init__(self, numTaps, mu):
         self.numTaps = numTaps
@@ -26,3 +33,48 @@ class LMSFilter:
         #return filtered output and error signal
         return yn, en
 
+#noise cancellation part:
+
+fs = 1000
+t = np.arange(0, 1, 1/fs)
+
+#source signal
+source = np.sin(2 * np.pi * 50 * t)
+
+#noise signal
+noise = 0.5 * np.random.randn(len(t))
+
+#desired signal
+desired = source + noise
+
+#reference signal
+reference = noise + 0.05 * np.random.randn(len(t))
+
+
+#filter setup
+N = 32
+mu = 0.01
+filter = LMSFilter(N, mu)
+
+#create output/error arrays
+output = np.zeros(len(t))
+error = np.zeros(len(t))
+
+for n in range(len(t)):
+    y, e = filter.update(reference[n], desired[n])
+    output[n] = y
+    error[n] = e
+
+#update error signal
+cleanedSignal = error
+
+#plot the graphs
+plt.figure()
+plt.plot(t, desired, label="Noisy Signal")
+plt.plot(t, cleanedSignal, label="LMS Filtered Signal")
+plt.plot(t, source, label="Source Signal")
+plt.xlabel("time (s)")
+plt.ylabel("amplitude")
+plt.legend()
+plt.title("LMS Adaptive Noise Cancellation")
+plt.show()
