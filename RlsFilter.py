6f7a739a9ca8e8d81e513a017550019a12c5d8ed
alfr3dexe <alfredcastaneda32@gmail.com>
2026-06-29 19:18:40 -0700

    Updated with graphs for each signal

--- RlsFilter.py
+++ RlsFilter.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import librosa
 
-clas RLS:
+class RLS:
 	def __init__(self, n_features, forgetting= 0.99, delta=1000):
 		self.lambda_ = forgetting
 		self.w = np.zeroes((n_features, 1))
@@ -26,3 +28,42 @@ clas RLS:
 		self.P = (self.P - g @ x.T @ self.P) / self.lambda_
 
 		return y, e
+
+fs = 1000 
+t = np.linspace(0, 1, fs)
+
+clean = np.sin(2 * np.pi * 5 * t)
+
+noise = 0.3 * np.random.randn(len(t))
+
+noisy = clean + noise
+
+
+
+#plot the graphs
+
+# Clean signal
+plt.figure(figsize=(8,4))
+plt.plot(t, clean)
+plt.title("Clean Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+
+# Noise
+plt.figure(figsize=(8,4))
+plt.plot(t, noise)
+plt.title("Noise Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+
+# Noisy signal
+plt.figure(figsize=(8,4))
+plt.plot(t, noisy)
+plt.title("Noisy Signal")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+
+plt.show()
